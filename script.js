fb83076730c9ba4b62613112a9d650434bc15327
Alexander Bailey <alexkingstonbailey@gmail.com>
2020-02-17 15:16:53 +1300

    change size  and colour  with space press

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+function generatePlanet() { 
+    let planet = document.getElementById("planet")
+
+    let size = Math.random() * 500 + 50;
+    planet.style.height = size;
+    planet.style.width = size;
+
+    let colours = Array("#ebdbb2", "#cc241d", "#458588", "#98971a", "#b16286");
+    let colour = colours[Math.floor(Math.random() * colours.length)];
+    planet.style.backgroundColor = colour;
+}
+
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
-        // alert("space pressed")
+        generatePlanet()
     }
 }
